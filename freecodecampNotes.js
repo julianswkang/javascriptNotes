9684d27a00919360ee569b4751fe4edeb13bb674
Julian Kang <julianswkang@gmail.com>
2021-12-29 16:11:10 -0500

    oop with mixins

--- freecodecampNotes.js
+++ freecodecampNotes.js
@@ -737,3 +737,89 @@ Bird.prototype.fly = function() {
   console.log("I'm flying!");
 };
 //Now instances of Bird will have both eat() and fly() methods:
+
+//there are cases when inheritance is not the best solution. Inheritance does not work well for unrelated objects like Bird and Airplane. 
+//They can both fly, but a Bird is not a type of Airplane and vice versa.
+//For unrelated objects, it's better to use mixins. A mixin allows other objects to use a collection of functions.
+let flyMixin = function(obj) { //The flyMixin takes any object and gives it the fly method.
+  obj.fly = function() {
+    console.log("Flying, wooosh!");
+  }
+};
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+let plane = {
+  model: "777",
+  numPassengers: 524
+};
+flyMixin(bird);
+flyMixin(plane);
+//Here bird and plane are passed into flyMixin, which then assigns the fly function to each object. Now bird and plane can both fly:
+bird.fly(); //Flying, wooosh!
+plane.fly();//Flying, wooosh!
+
+//In the previous challenge, bird had a public property name. It is considered public because it can be accessed and 
+//changed outside of bird's definition.
+//bird.name = "Duffy";
+//Therefore, any part of your code can easily change the name of bird to any value. 
+//Think about things like passwords and bank accounts being easily changeable by any part of your codebase. 
+//That could cause a lot of issues.
+//The simplest way to make this public property private is by creating a variable within the constructor function. 
+//This changes the scope of that variable to be within the constructor function versus available globally. 
+//This way, the variable can only be accessed and changed by methods also within the constructor function.
+function Bird() {
+  let hatchedEgg = 10; //hatchedEgg is a variable created within the constructor function, no longer available globally
+  this.getHatchedEggCount = function() { //this method is priviledged, has access to private variable hatchedEgg
+    return hatchedEgg;
+  };
+}
+let ducky = new Bird();
+ducky.getHatchedEggCount(); //will return 10
+//Here getHatchedEggCount is a privileged method, because it has access to the private variable hatchedEgg. 
+//This is possible because hatchedEgg is declared in the same context as getHatchedEggCount. 
+//In JavaScript, a function always has access to the context in which it was created. This is called closure.
+
+//A common pattern in JavaScript is to execute a function as soon as it is declared:
+//This pattern is known as an immediately invoked function expression or IIFE.
+(function () {
+  console.log("Chirp, chirp!");
+})();
+//This is an anonymous function expression that executes right away, and outputs Chirp, chirp! immediately.
+//Note that the function has no name and is not stored in a variable. 
+//The two parentheses () at the end of the function expression cause it to be immediately executed or invoked. 
+
+//An immediately invoked function expression (IIFE) is often used to group related functionality into a single object or module. 
+//For example, an earlier challenge defined two mixins:
+function glideMixin(obj) {
+  obj.glide = function() {
+    console.log("Gliding on the water");
+  };
+}
+function flyMixin(obj) {
+  obj.fly = function() {
+    console.log("Flying, wooosh!");
+  };
+}
+//We can group these mixins into a module as follows:
+let motionModule = (function () {
+  return {
+    glideMixin: function(obj) {
+      obj.glide = function() {
+        console.log("Gliding on the water");
+      };
+    },
+    flyMixin: function(obj) {
+      obj.fly = function() {
+        console.log("Flying, wooosh!");
+      };
+    }
+  }
+})();
+//Note that you have an immediately invoked function expression (IIFE) that returns an object motionModule. 
+//This returned object contains all of the mixin behaviors as properties of the object. 
+//The advantage of the module pattern is that all of the motion behaviors can be packaged into a single object 
+//that can then be used by other parts of your code. Here is an example using it:
+motionModule.glideMixin(duck);
+duck.glide();
